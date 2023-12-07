5b5b8ea12740b9cb509ed7443dcac9ca0e4a48ee
martyasbutkus <noobtuberis1990@gmail.com>
2023-12-08 00:20:15 +0200

    aas

---
+++ script.js
@@ -0,0 +1,91 @@
+const display = document.querySelector("[data-current-operand]")
+const previousDisplay = document.querySelector("[data-previous-operand]")
+const dataNumber = document.querySelectorAll("[data-number]")
+const dataOperation = document.querySelectorAll("[data-operation]")
+const dataEquals = document.querySelector("[data-equals]")
+
+class Calculator {
+    constructor() {
+        this.lastResult = 0
+    }
+    
+    Condition() {
+        dataNumber.forEach((button) => {
+            button.addEventListener("click", () => {
+                switch (button.innerText) {
+                    default:
+                        display.innerText += button.innerText
+                }
+            })
+        })
+        
+        dataOperation.forEach((operation) => {
+            operation.addEventListener("click", () => {
+                const lastChar = display.innerText.slice(-1)
+                const isOperator = ["+", "-", "*", "÷"].includes(lastChar)
+                
+                switch (operation.innerText) {
+                    case "+":
+                    case "-":
+                    case "*":
+                    case "÷":
+                        if (display.innerText.length && !isOperator) {
+                            display.innerText += "" + operation.innerText + " "
+                        }
+                        break
+                    case "AC":
+                        display.innerText = ""
+                        break
+                    case 'DEL':
+                        if (display.innerText) {
+                            display.innerText = display.innerText.slice(0, -1)
+                        }
+                        break
+                    default:
+                        display.innerText += operation.innerText
+                }
+            })
+        })
+    }
+    Equals() {
+        dataEquals.addEventListener("click", () => {
+            if (display.innerText.length) {
+                let condition = display.innerText
+                let operator = condition.match(/[\+\-\*\/]/g)
+                let operators = condition.split(/[\+\-\*\/]/).map(parseFloat)
+                
+                if (operator && operators.length >= 2) {
+                    let result = operators[0]
+                    for (let i = 0; i < operator.length; i++) {
+                        switch (operator[i]) {
+                            case "+":
+                                result += operators[i + 1]
+                                break
+                            case "-":
+                                result -= operators[i + 1]
+                                break
+                            case "*":
+                                result *= operators[i + 1]
+                                break
+                            case "÷":
+                                result /= operators[i + 1]
+                                break
+                        }
+                    }
+                    console.log(result)
+                    this.lastResult = result
+                    previousDisplay.innerText = result
+                    display.innerText = result
+
+                }
+            }
+        })
+    }
+    Localstorage(){
+        localStorage.setItem('lastResult', this.result)
+    }
+}
+
+const calculation = new Calculator()
+calculation.Condition()
+calculation.Equals()
